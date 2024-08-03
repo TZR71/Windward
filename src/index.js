c3a1502f420efd8c30bef53b0d0eb7c62b662750
TZR71 <Tiinarojola@gmail.com>
2024-08-03 18:10:46 +0200

    Added search-functionality using API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
+function refreshWeatherInfo(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "eb0432d8499otf6b1e6a9e4fe11cf387";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeatherInfo);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML=searchInput.value;
+
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Sydney");
